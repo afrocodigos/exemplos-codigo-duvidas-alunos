455b861efbffc41e2a3f2f710a9832b146973bd4
Jéss <oskojess.com.br>
2024-03-15 13:43:46 -0300

    adicionar exemplo de indexado/não indexado

---
+++ lista_dicionario_indexada.py
@@ -0,0 +1,15 @@
+# Exemplo de lista indexada
+lista = ['a', 'b', 'c', 'd', 'e']
+
+# Acessando elementos da lista por índice
+print("Elemento no índice 0 da lista:", lista[0])
+print("Elemento no índice 2 da lista:", lista[2])
+print("Elemento no índice 4 da lista:", lista[4])
+
+# Exemplo de dicionário não indexado
+dicionario = {'chave1': 10, 'chave2': 20, 'chave3': 30}
+
+# Acessando elementos do dicionário por chave
+print("\nValor associado à chave 'chave1':", dicionario['chave1'])
+print("Valor associado à chave 'chave2':", dicionario['chave2'])
+print("Valor associado à chave 'chave3':", dicionario['chave3'])
