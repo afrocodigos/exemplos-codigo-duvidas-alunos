d1fdef972444860b9a6dbbf19d2d89678ae18c3b
Jéss <oskojess.com.br>
2024-03-15 13:47:33 -0300

    add exemplos elif

---
+++ elif.py
@@ -0,0 +1,8 @@
+idade = 10
+
+if idade >= 14:
+    print("Você pode participar do grupo de jovens!")
+elif idade < 0:
+    print("Idade inválida")
+else:
+    print("Você é muito jovem para participar do grupo de jovens.")
